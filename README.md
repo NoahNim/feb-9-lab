fa9ca019b85e8bffdf013c5fd95e453f2bd61516
Noah <nmedoff123@gmail.com>
2016-02-09 10:30:42 -0800

    Edited readme and added two files

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # feb-9-lab
-# feb-9-lab
-# feb-9-lab
+
+This is a repo for day 2 lab
---
+++ app.js
@@ -0,0 +1,24 @@
+// JS file for Feb 8 class demo
+
+var userName = prompt('What is your name?');
+console.log(userName);
+
+alert("Hello there, " + userName);
+console.log(userName + ' was a man who thought he was a loner');
+
+var questionOne = prompt('The NFL team at the bottom comes in?');
+console.log('But he knew it wouldn\'t ' + questionOne);
+
+alert('Yes it comes in ' + questionOne);
+
+var homeTown = prompt('What are you from?');
+console.log(userName + ' left his home in ' + homeTown + ', Arizona');
+
+var whatState = prompt('What state is Los Angeles in?');
+console.log('For some ' + whatState + ' grass');
+
+var whyAreYou = prompt('Why are you here?');
+console.log(whyAreYou);
+
+alert('Too bad! Get back home!');
+console.log('Get back, get back. Get back to where you once belonged');
